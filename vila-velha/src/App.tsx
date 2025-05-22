d8f25d7414268d1b73975ea5b3f4afec299ecf6b
AdhemarMolon <162194104+AdhemarMolon@users.noreply.github.com>
2025-05-22 12:17:27 -0300

    Fazendo o esqueleto do projeto novamente
    O taildwind css estava muito bugado, o projeto antigo estava uma loucura, fiz tudo de dnv e agora está funcionando ;)

---
+++ vila-velha/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+      <h1 className="text-4xl font-retro text-yellow-800">Tailwind tá VIVO! 🚗🔥</h1>
+    </div>
+  );
+}
